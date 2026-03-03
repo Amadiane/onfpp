d54dceef89f852879e6e9266e1e38f9a788cf4c0
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-03-03 12:15:04 +0000

    error

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -112,9 +112,12 @@ AUTH_USER_MODEL = 'Base.User'
 # CORS (React Frontend)
 # -----------------------------
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
+    "https://onfpp.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+
 ]
 
 
